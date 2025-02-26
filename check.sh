f0191b314efdd0f86c3cfd7a00dfc89a3557bb0e
Rsslone <rsslone1120@gmail.com>
2025-02-26 05:57:58 -0500

    Create check.sh

---
+++ check.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+SERVICE_NAME="$1"
+
+if [ -z "$SERVICE_NAME" ]; then
+    echo "Usage: $0 <service_name>"
+    exit 1
+fi
+
+STATUS=$(/opt/etc/init.d/rc.unslung check "$SERVICE_NAME")
+
+echo "$STATUS" | grep -q "dead"
+if [ $? -eq 0 ]; then
+    echo "$SERVICE_NAME is dead. Attempting to start..."
+    /opt/etc/init.d/rc.unslung start "$SERVICE_NAME"
+fi
